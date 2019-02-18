0acc3c490e2ad52fa2d57c5928007ba5a7df0b8c
yoyoyo <naga.yoshi.yoshi@gmail.com>
2019-02-19 08:51:23 +0900

    fixed

--- Question_semaseg/answers/easy_pytorch.py
+++ Question_semaseg/answers/easy_pytorch.py
@@ -6,8 +6,8 @@ import numpy as np
 from glob import glob
 
 num_classes = 2
-img_height, img_width = 572, 572#572, 572
-out_height, out_width = 388, 388#388, 388
+img_height, img_width = 128, 128#572, 572
+out_height, out_width = 128, 128#388, 388
 GPU = True
 torch.manual_seed(0)
 
@@ -26,85 +26,81 @@ class Mynet(torch.nn.Module):
 
         enc1 = []
         for i in range(2):
-            f = 3 if i == 0 else 64
-            enc1.append(torch.nn.Conv2d(f, 64, kernel_size=3, padding=0, stride=1))
-            enc1.append(torch.nn.BatchNorm2d(64))
+            f = 3 if i == 0 else 32
+            enc1.append(torch.nn.Conv2d(f, 32, kernel_size=3, padding=1, stride=1))
+            enc1.append(torch.nn.BatchNorm2d(32))
             enc1.append(torch.nn.ReLU())
         self.enc1 = torch.nn.Sequential(*enc1)
 
         enc2 = []
         for i in range(2):
-            f = 64 if i == 0 else 128
-            enc2.append(torch.nn.Conv2d(f, 128, kernel_size=3, padding=0, stride=1))
-            enc2.append(torch.nn.BatchNorm2d(128))
+            f = 32 if i == 0 else 32
+            enc2.append(torch.nn.Conv2d(f, 32, kernel_size=3, padding=1, stride=1))
+            enc2.append(torch.nn.BatchNorm2d(32))
             enc2.append(torch.nn.ReLU())
         self.enc2 = torch.nn.Sequential(*enc2)
 
         enc3 = []
         for i in range(2):
-            f = 128 if i == 0 else 256
-            enc3.append(torch.nn.Conv2d(f, 256, kernel_size=3, padding=0, stride=1))
-            enc3.append(torch.nn.BatchNorm2d(256))
+            f = 32 if i == 0 else 64
+            enc3.append(torch.nn.Conv2d(f, 64, kernel_size=3, padding=1, stride=1))
+            enc3.append(torch.nn.BatchNorm2d(64))
             enc3.append(torch.nn.ReLU())
         self.enc3 = torch.nn.Sequential(*enc3)
 
         enc4 = []
         for i in range(2):
-            f = 256 if i == 0 else 512
-            enc4.append(torch.nn.Conv2d(f, 512, kernel_size=3, padding=0, stride=1))
-            enc4.append(torch.nn.BatchNorm2d(512))
+            f = 64 if i == 0 else 128
+            enc4.append(torch.nn.Conv2d(f, 128, kernel_size=3, padding=1, stride=1))
+            enc4.append(torch.nn.BatchNorm2d(128))
             enc4.append(torch.nn.ReLU())
         self.enc4 = torch.nn.Sequential(*enc4)
 
         enc5 = []
         for i in range(2):
-            f = 512 if i == 0 else 1024
-            enc5.append(torch.nn.Conv2d(f, 1024, kernel_size=3, padding=0, stride=1))
-            enc5.append(torch.nn.BatchNorm2d(1024))
+            f = 128 if i == 0 else 128
+            enc5.append(torch.nn.Conv2d(f, 128, kernel_size=3, padding=1, stride=1))
+            enc5.append(torch.nn.BatchNorm2d(128))
             enc5.append(torch.nn.ReLU())
         self.enc5 = torch.nn.Sequential(*enc5)
 
-        self.tconv4 = torch.nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.tconv4_bn = torch.nn.BatchNorm2d(512)
+        self.tconv4 = torch.nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
         dec4 = []
         for i in range(2):
-            f = 1024 if i == 0 else 512
-            dec4.append(torch.nn.Conv2d(f, 512, kernel_size=3, padding=0, stride=1))
-            dec4.append(torch.nn.BatchNorm2d(512))
+            f = 256 if i == 0 else 64
+            dec4.append(torch.nn.Conv2d(f, 64, kernel_size=3, padding=1, stride=1))
+            dec4.append(torch.nn.BatchNorm2d(64))
             dec4.append(torch.nn.ReLU())
         self.dec4 = torch.nn.Sequential(*dec4)
 
-        self.tconv3 = torch.nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.tconv3_bn = torch.nn.BatchNorm2d(256)
+        self.tconv3 = torch.nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
         dec3 = []
         for i in range(2):
-            f = 512 if i == 0 else 256
-            dec3.append(torch.nn.Conv2d(f, 256, kernel_size=3, padding=0, stride=1))
-            dec3.append(torch.nn.BatchNorm2d(256))
+            f = 64 if i == 0 else 64
+            dec3.append(torch.nn.Conv2d(f, 64, kernel_size=3, padding=1, stride=1))
+            dec3.append(torch.nn.BatchNorm2d(64))
             dec3.append(torch.nn.ReLU())
         self.dec3 = torch.nn.Sequential(*dec3)
 
-        self.tconv2 = torch.nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.tconv2_bn = torch.nn.BatchNorm2d(128)
+        self.tconv2 = torch.nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
         dec2 = []
         for i in range(2):
-            f = 256 if i == 0 else 128
-            dec2.append(torch.nn.Conv2d(f, 128, kernel_size=3, padding=0, stride=1))
-            dec2.append(torch.nn.BatchNorm2d(128))
+            f = 96 if i == 0 else 32
+            dec2.append(torch.nn.Conv2d(f, 32, kernel_size=3, padding=1, stride=1))
+            dec2.append(torch.nn.BatchNorm2d(32))
             dec2.append(torch.nn.ReLU())
         self.dec2 = torch.nn.Sequential(*dec2)
 
         self.tconv1 = torch.nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.tconv1_bn = torch.nn.BatchNorm2d(64)
         dec1 = []
         for i in range(2):
-            f = 128 if i == 0 else 64
-            dec1.append(torch.nn.Conv2d(f, 64, kernel_size=3, padding=0, stride=1))
-            dec1.append(torch.nn.BatchNorm2d(64))
+            f = 64 if i == 0 else 32
+            dec1.append(torch.nn.Conv2d(f, 32, kernel_size=3, padding=1, stride=1))
+            dec1.append(torch.nn.BatchNorm2d(32))
             dec1.append(torch.nn.ReLU())
         self.dec1 = torch.nn.Sequential(*dec1)
 
-        self.out = torch.nn.Conv2d(64, num_classes+1, kernel_size=1, padding=0, stride=1)
+        self.out = torch.nn.Conv2d(32, num_classes+1, kernel_size=1, padding=0, stride=1)
         
         
     def forward(self, x):
@@ -116,7 +112,7 @@ class Mynet(torch.nn.Module):
         x_enc2 = self.enc2(x)
         x = F.max_pool2d(x_enc2, 2, stride=2, padding=0)
         
-        # block conv31
+        # block conv3
         x_enc3 = self.enc3(x)
         x = F.max_pool2d(x_enc3, 2, stride=2, padding=0)
         
@@ -127,24 +123,30 @@ class Mynet(torch.nn.Module):
         # block conv5
         x = self.enc5(x)
 
-        x = self.tconv4_bn(self.tconv4(x))
-
+        #x = self.tconv4(x)
+        #x = torch.nn.Upsample(scale_factor=2, mode='nearest')(x)
+        x = torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
         _x = crop_layer(x_enc4, x.size())
-
         x = torch.cat((_x, x), dim=1)
         x = self.dec4(x)
 
-        x = self.tconv3_bn(self.tconv3(x))
+        #x = self.tconv3(x)
+        #x = torch.nn.Upsample(scale_factor=2, mode='nearest')(x)
+        x = torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
         _x = crop_layer(x_enc3, x.size())
         x = torch.cat((_x, x), dim=1)
-        x = self.dec3(x)
+        x = self.dec3(x_enc3)
 
-        x = self.tconv2_bn(self.tconv2(x))
+        #x = self.tconv2(x)
+        #x = torch.nn.Upsample(scale_factor=2, mode='nearest')(x)
+        x = torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
         _x = crop_layer(x_enc2, x.size())
         x = torch.cat((_x, x), dim=1)
         x = self.dec2(x)
 
-        x = self.tconv1_bn(self.tconv1(x))
+        #x = self.tconv1(x)
+        #x = torch.nn.Upsample(scale_factor=2, mode='nearest')(x)
+        x = torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')
         _x = crop_layer(x_enc1, x.size())
         x = torch.cat((_x, x), dim=1)
         x = self.dec1(x)
@@ -214,7 +216,7 @@ def train():
 
     # model
     model = Mynet().to(device)
-    opt = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    opt = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     model.train()
 
     xs, ts, paths = data_load('../Dataset/train/images/', hf=True, vf=True)
@@ -226,7 +228,7 @@ def train():
     np.random.seed(0)
     np.random.shuffle(train_ind)
     
-    for i in range(1000):
+    for i in range(2000):
         if mbi + mb > len(xs):
             mb_ind = train_ind[mbi:]
             np.random.shuffle(train_ind)
@@ -287,10 +289,10 @@ def test():
         out = np.zeros((out_height, out_width, 3), dtype=np.uint8)
         for i, (_, vs) in enumerate(CLS.items()):
             out[pred == (i+1)] = vs
-        
+   
         import matplotlib.pyplot as plt
         plt.subplot(1,2,1)
-        plt.imshow(x.detach().cpu().numpy()[0].transpose(1,2,0)[...,::-1])
+        plt.imshow(x.detach().cpu().numpy()[0].transpose(1,2,0)[..., ::-1])
         plt.subplot(1,2,2)
         plt.imshow(out[..., ::-1])
         plt.show()
